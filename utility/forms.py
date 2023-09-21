378603b2012a0a2757f1818f84e240e7f8a07405
Krushovice <kickstar69@yandex.ru>
2023-09-21 04:57:01 +0000

    Добавлен вывод ошибок в форме авторизации

--- utility/forms.py
+++ utility/forms.py
@@ -3,7 +3,10 @@ from wtforms import SearchField, SubmitField, BooleanField, PasswordField
 from wtforms.validators import DataRequired, Email, Length
 
 class LoginForm(FlaskForm):
-    email = SearchField("Email: ", validators=[Email()])
-    psw = PasswordField("Пароль: ", validators=[DataRequired()])
+    email = SearchField("Email: ", validators=[Email("Некорректный email")])
+    psw = PasswordField("Пароль: ", validators=[DataRequired(),
+                                                Length(min=4,
+                                                       max=100,
+                                                       message="Пароль должен быть от 4 до 100 символов")])
     remember = BooleanField("Запомнить", default=False)
     submit = SubmitField("Войти")
