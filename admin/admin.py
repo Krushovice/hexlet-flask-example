f45fc24d5a065733e7cf7359162fb56c294db50d
Krushovice <kickstar69@yandex.ru>
2023-09-22 02:33:43 +0000

    Добавлена авторизация админа и главная страница

--- admin/admin.py
+++ admin/admin.py
@@ -1,3 +1,52 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect, url_for, flash, session
 
-admin = Blueprint('admin', __name__, template_folder='templates', static_folder='static')
+admin = Blueprint('admin', __name__,
+                  template_folder='templates',
+                  static_folder='static')
+
+
+def login_admin():
+    session['admin_logged'] = 1
+
+
+def isLogged():
+    return True if session.get('admin_logged') else False
+
+
+def logout_admin():
+    session.pop('admin_logged', None)
+
+menu = [{'url': '.index', 'title': 'Панель'},
+        {'url': '.logout', 'title': 'Выйти'}]
+
+@admin.route('/')
+def index():
+    if not isLogged():
+        return redirect(url_for('.login'))
+    return render_template('admin/index.html',
+                           menu=menu,
+                           title='Админ-панель')
+
+
+@admin.route('/login', methods=["POST", "GET"])
+def login():
+    if isLogged():
+        return redirect(url_for('.index'))
+
+    if request.method == "POST":
+        data = request.form.to_dict()
+        if data['user'] == "admin" and data['psw'] == "123456":
+            login_admin()
+            return redirect(url_for('.index'))
+        else:
+            flash("Неверная пара логин/пароль", "error")
+    return render_template('admin/login.html', title="Админ-панель")
+
+
+@admin.route('/logout', methods=["POST", "GET"])
+def logout():
+    if not isLogged():
+        return redirect(url_for('.login'))
+
+    logout_admin()
+    return redirect(url_for('.login'))
